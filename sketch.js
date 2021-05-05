b6bf2b5728c3da0b4bd58019a0c13adf55095963
whjrcoder <anklet.007@gmail.com>
2021-05-05 12:46:05 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+platform=new Ground(150,305,300,170)
     bird = new Bird(100,100);
 
 }
@@ -57,6 +57,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+platform.display();
     bird.display();
 }
